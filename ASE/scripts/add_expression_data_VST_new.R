9333aeba00777d03126583a641b53cef799305b6
robbedewin <robbedewin@hotmail.com>
2024-12-07 14:51:31 +0100

    Add new script for VST normalization and log2 fold-change analysis of expression data; includes sample filtering, p-value combination, and output generation for each sample

---
+++ ASE/scripts/add_expression_data_VST_new.R
@@ -0,0 +1,125 @@
+.libPaths("/staging/leuven/stg_00096/home/rdewin/system/miniconda/envs/ASE_R/lib/R/library")
+
+# Load the required libraries
+library(DESeq2)
+library(GenomicFeatures)
+library(ggplot2)
+
+### My combined counts file from STAR output
+counts_file <- "/staging/leuven/stg_00096/home/rdewin/RNA/results/counts/combined_counts.tsv"
+
+# Load counts matrix
+counts_data <- read.delim(counts_file, header = TRUE, row.names = 1, as.is = TRUE)
+
+# List of matched samples
+matchedSamples <- c("P011", "P013", "P016", "P017", "P018", "P019", "P020", "P022", "P023", "P024", "P026", "P028", "P029", "P033", "P037", "P041", "P057", "P058", "P059", "P060", "P061", "P064", "P065", "P066", "P086", "P103", "P105")
+
+# Filter counts dataframe
+filtered_counts_data <- counts_data[, colnames(counts_data) %in% matchedSamples]
+
+sampleTable <- data.frame(sample = colnames(filtered_counts_data),
+                          condition = "T-ALL")
+
+# Create DESeq2 object from counts matrix
+dds <- DESeqDataSetFromMatrix(countData = filtered_counts_data, 
+                              colData = sampleTable, 
+                              design = ~1)
+
+# Normalize and perform variance-stabilizing transformation (VST)
+dds <- estimateSizeFactors(dds)
+dds_vst <- vst(dds, blind = TRUE)
+
+# Compute mean expression and log2 fold-change relative to "virtual mean sample"
+dds_vstmeans <- rowMeans(assay(dds_vst))
+vst_fc <- assay(dds_vst) - dds_vstmeans
+
+# Plot mean expression vs log2 fold-change (to start httpgd server, run `hgd()`)
+plot(dds_vstmeans, vst_fc[, 1], main = "Log2 Fold Change vs Mean Expression")
+plot(log10(rowMeans(counts(dds, normalized=TRUE))+1), vst_fc[,1])
+
+# Save normalized and VST-transformed data
+write.table(assay(dds_vst), 
+            file = "/staging/leuven/stg_00096/home/rdewin/ASE/expression_data/RNAcounts_vst.txt", 
+            quote = FALSE, sep = "\t", row.names = TRUE, col.names = TRUE)
+
+# Save log2 fold-change data
+l2fcdf <- as.data.frame(vst_fc)
+l2fcdf$gene_name <- rownames(l2fcdf)
+write.table(l2fcdf, 
+            file = "/staging/leuven/stg_00096/home/rdewin/ASE/expression_data/RNAlog2fc_vst.txt", 
+            quote = FALSE, sep = "\t", row.names = TRUE, col.names = TRUE)
+
+# Save normalized counts data
+resdf <- as.data.frame(counts(dds, normalized=TRUE))
+
+## Combine p-values (of powered SNP loci) per gene
+
+## Functions
+combine_pvals <- function(ase_pergene) {
+  # ase_pergene <- ase_results_annot[4:14, ]
+  outdf <- data.frame(contig = ase_pergene[1, "contig"], positions = paste0(unique(ase_pergene$position), collapse = ","), pcombined = 1, gene = ase_pergene[1, "gene"], stringsAsFactors = F, power = T)
+  
+  is_duplicated <- duplicated(ase_pergene$position)
+  has_power <- ase_pergene$filter <= 0.01
+  ase_pergene <- ase_pergene[!is_duplicated & has_power, ]
+  
+  if (nrow(ase_pergene) > 1) {
+    outdf$pcombined <- fishersMethod(x = ase_pergene$pval)
+  } else if (nrow(ase_pergene) == 1) {
+    outdf$pcombined <- ase_pergene$pval
+  } else {
+    outdf$power <- F
+  }
+  return(outdf)
+}
+
+fishersMethod <- function(x) {
+  pchisq(q = -2 * sum(log(x)), df = 2*length(x), lower.tail = F)
+}
+
+
+# Load gene annotations
+gtffile <- "/staging/leuven/stg_00096/home/rdewin/WGS/resources/annotation.gtf"
+hstxdb <- makeTxDbFromGFF(file = gtffile, organism = "Homo sapiens")
+#seqlevels(hstxdb) <- sub(pattern = "chr", replacement = "", x = seqlevels(seqinfo(hstxdb)))
+hsexondb <- exons(x = hstxdb, columns = c("gene_id"))
+
+# For loop to loop over the sampleIDs
+for (SAMPLEID in matchedSamples) {
+  SAMPLEID <- "P011"
+  print(paste("Processing sample:", SAMPLEID))
+  
+  # Read the ASE results file for each sample
+  ase_resultsfile <- paste0("/staging/leuven/stg_00096/home/rdewin/ASE/results/", SAMPLEID, "/", SAMPLEID, "_asereadcounts_nomatch_pvals_annotated.tsv")
+  ase_results <- read.delim(ase_resultsfile, header = TRUE, as.is = TRUE)
+  
+  # make results into GRanges object, identify all exonic SNPs and create new df with all of these (contains duplicate SNPs)
+  asegr <- GRanges(seqnames = ase_results$contig, ranges = IRanges(start = ase_results$position, end = ase_results$position))
+  annothits <- findOverlaps(query = asegr, subject = hsexondb)
+  
+  # in one case, there were two genes using the same exon ... this just takes the first
+  hitgenes <- sapply(mcols(hsexondb[subjectHits(annothits)])$gene_id, FUN = function(x) x[[1]])
+  ase_results_annot <- data.frame(ase_results[queryHits(annothits), colnames(ase_results) != "gene"], gene = hitgenes, stringsAsFactors = F)
+  
+  # create output dataframe with combined p-value per gene + adjust for multiple testing
+  outdf <- do.call(rbind, by(data = ase_results_annot, INDICES = ase_results_annot$gene, FUN = combine_pvals))
+  outdf$padj <- 1
+  outdf[outdf$power, "padj"] <- p.adjust(p = outdf[outdf$power, "pcombined"], method = "fdr")
+  
+  # add in the log2-fold change data and actual gene names
+  outdf[, c("log2fc", "mean_expression", "gene_name")] <- l2fcdf[outdf$gene, c(grep(pattern = paste0(sub(pattern = "-", replacement = ".", SAMPLEID), "$"), x = colnames(l2fcdf), value = T), "mean_expression", "gene_name")]
+
+
+    # format
+    outdf$contig <- factor(outdf$contig, levels = c(1:22, "X"))
+    outdf <- outdf[order(outdf$contig, as.integer(unlist(lapply(strsplit(outdf$positions, split = ","), FUN = function(x) x[1])))), ]
+    outfile <- paste0("/staging/leuven/stg_00096/home/rdewin/ASE/results/", SAMPLEID, "/", SAMPLEID, "_imbalance_expression_vst.txt")
+    write.table(x = outdf, file = outfile, quote = F, sep = "\t", row.names = F, col.names = T)
+
+    # plot
+    p1 <- plot_imbalance_expression(imbalancedf = outdf)
+    plotfile <- paste0("/staging/leuven/stg_00096/home/rdewin/ASE/results/", SAMPLEID, "/", SAMPLEID, "_imbalance_expression_vst.png")
+    ggsave(plotfile, p1)
+    
+}
+
